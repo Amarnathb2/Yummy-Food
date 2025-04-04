a254bc675a618c77ec585c23cc10b05f64994fff
HP <HP@DESKTOP-FTAKCEA>
2025-04-04 21:49:43 +0530

    resert password configured

---
+++ YummyFood/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YummyFood.Models
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "Password must be at least {2} characters long.", MinimumLength = 6)]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
